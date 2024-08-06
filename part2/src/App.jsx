fcef0f2faa948a7906dd83f56af6aa6a1eb80c5d
salehalakhras <93873314+salehalakhras@users.noreply.github.com>
2024-08-06 16:03:51 +0300

    finish part2  2.11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import Form from './components/Form'
-import Persons from './components/Persons'
-import Filter from './components/Filter'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Form from './components/Form';
+import Persons from './components/Persons';
+import Filter from './components/Filter';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [shownPersons, setShownPersons] = useState(persons);
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons/')
+    .then((response) => {
+      setPersons(response.data)
+      setShownPersons(response.data)
+    });
+  },[])
   return (
     <div>
       <h2>Phonebook</h2>
@@ -24,4 +27,5 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
+
